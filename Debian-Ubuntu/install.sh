9849f0c01bc624441aab99b0121f2a04af6f7189
Logilype <logilype@gmail.com>
2023-10-22 12:46:38 +0200

    first commit

---
+++ Debian-Ubuntu/install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Pulled from the official website: https://docs.docker.com/engine/install/debian/ and https://docs.docker.com/compose/install/linux/
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# Install the latest version
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Test Docker with Hello World and run `docker compose version` to test Docker Compose
+sudo docker run hello-world
+docker compose version

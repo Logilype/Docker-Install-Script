93dadd48d9238c46443a042cda4812882377c0db
Logilype <79409424+Logilype@users.noreply.github.com>
2023-12-03 20:28:18 +0100

    Add files via upload

--- Debian-Ubuntu/install.sh
+++ Debian-Ubuntu/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Pulled from the official website: https://docs.docker.com/engine/install/debian/ and https://docs.docker.com/compose/install/linux/
+# Pulled from the official website: https://docs.docker.com/engine/install/ubuntu/
 
 # Ensure that sudo and curl are installed on the system (run as root)
 # apt install sudo curl
@@ -9,12 +9,12 @@
 sudo apt-get update
 sudo apt-get install ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
 # Add the repository to Apt sources:
 echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
